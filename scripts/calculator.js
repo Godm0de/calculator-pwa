ac6fb62c80d553fb37515aeec62bfd7730a2e40f
Godm0de <>
2021-07-19 23:15:28 +0200

    Fix conflig in js file

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -13,19 +13,11 @@ const options = {
     "RESET": () => {  buffer = 0; display.value = ""; }
 }
 
-<<<<<<< HEAD
-const commonKeys =  (value) => { display.value = (resultBool) ? value : ( display.value + value ); resultBool = false;}
-
-document.getElementById("keypad").addEventListener( "click", (e) => {
-    try{
-        (e.toElement.className === "key") ? (options[e.target.defaultValue] ? options[e.target.defaultValue]() : commonKeys(e.target.defaultValue)) : '';
-=======
 const buttonsDefault =  (value) => {  display.value = (resultBool) ? value : ( display.value + value ); resultBool = false; }
 
 document.getElementById("keypad").addEventListener( "click", (e) => {
     try{
         (e.target.classList.contains("key")) ? (buttons[e.target.innerText] ? buttons[e.target.innerText]() : buttonsDefault(e.target.innerText)) : null;
->>>>>>> develop
      }catch(e){
          if(e instanceof SyntaxError){
              buffer = 0;
