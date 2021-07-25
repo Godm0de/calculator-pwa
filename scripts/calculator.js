375caf875881cccbc684a3fb6d755034d4dd2927
Godm0de <>
2021-07-25 10:43:52 +0200

    Fix typo in calculator.js

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -10,11 +10,11 @@ const options = {
     "RESET": () => { display.value = ""; }
 }
 
-const buttonsDefault =  (value) => {  display.value += value }
+const optionsDefault =  (value) => {  display.value += value }
 
 document.getElementById("keypad").addEventListener( "click", (e) => {
     try{
-        (e.target.classList.contains("key")) ? (buttons[e.target.innerText] ? buttons[e.target.innerText]() : buttonsDefault(e.target.innerText)) : null;
+        (e.target.classList.contains("key")) ? (options[e.target.innerText] ? options[e.target.innerText]() : optionsDefault(e.target.innerText)) : null;
      }catch(e){
          if(e instanceof SyntaxError){
              buffer = 0;
